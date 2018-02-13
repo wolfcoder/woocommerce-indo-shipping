522d0e3ba4404a5359c189f98e0108927f72b6de
bbg2021@gmail.com <@bbg212121>
2018-02-13 20:52:40 +0800

    refactoring css structure

--- admin/init-zones.php
+++ admin/init-zones.php
@@ -1,156 +1,161 @@
 <?php
+
 /*
-  Zone setting for Indo Shipping
+	Zone setting for Indo Shipping
 */
+
 class WCIS_Zones_Method extends WC_Shipping_Method {
-  private $api;
-  private $main_settings;
+	private $api;
+	private $main_settings;
 
-  public function __construct($instance_id = 0) {
-		$this->id = 'wcis_zone';
-    $this->instance_id = absint($instance_id);
+	public function __construct( $instance_id = 0 ) {
+		$this->id          = 'wcis_zone';
+		$this->instance_id = absint( $instance_id );
 
-    $this->title = __('Indo Shipping', 'wcis');
-		$this->method_title = __('Indo Shipping', 'wcis');
-    $this->method_description = __('Indonesian domestic shipping with JNE, TIKI, or POS', 'wcis');
-    $this->supports = array('shipping-zones', 'instance-settings',);
+		$this->title              = __( 'Indo Shipping', 'wcis' );
+		$this->method_title       = __( 'Indo Shipping', 'wcis' );
+		$this->method_description = __( 'Indonesian domestic shipping with JNE, TIKI, or POS', 'wcis' );
+		$this->supports           = array( 'shipping-zones', 'instance-settings' );
 
-    // global
-    $this->main_settings = get_option('woocommerce_wcis_settings');
-    $this->api = new WCIS_API($this->main_settings['key']);
+		// global
+		$this->main_settings = get_option( 'woocommerce_wcis_settings' );
+		$this->api           = new WCIS_API( $this->main_settings['key'] );
 
-    // allow save setting
-    add_action('woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
+		// allow save setting
+		add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
 	}
 
 	/*
-	  Calculate_shipping function.
-
-	  @param mixed $package
+	 * Calculate_shipping function.
+	 * @param mixed $package
 	*/
-	function calculate_shipping($package = array() ) {
-    // if district not exists or empty
-    $id_exists = array_key_exists('destination_id', $package['destination']);
-    if(!$id_exists || empty($package['destination']['destination_id']) ) {
-      return false;
-    }
 
-    $costs = $this->_get_costs($package);
-    $this->_set_rate($costs);
+	function calculate_shipping( $package = array() ) {
+		// if district not exists or empty
+		$id_exists = array_key_exists( 'destination_id', $package['destination'] );
+		if ( ! $id_exists || empty( $package['destination']['destination_id'] ) ) {
+			return false;
+		}
+
+		$costs = $this->_get_costs( $package );
+		$this->_set_rate( $costs );
 	}
 
-  /////
+	/////
 
 
-  /*
-    Get the costs from selected Couriers.
+	/*
+	 *Get the costs from selected Couriers.
+	 * @param array $package - The shipping detail
+	 * @return array - List of cost grouped by each courier
+	*/
+	private function _get_costs( $package ) {
+		$weight            = $this->_calculate_weight( $package );
+		$selected_couriers = $this->_get_selected_couriers();
 
-    @param array $package - The shipping detail
-    @return array - List of cost grouped by each courier
-  */
-  private function _get_costs($package) {
-    $weight = $this->_calculate_weight($package);
-    $selected_couriers = $this->_get_selected_couriers();
+		$args = array(
+			'origin'      => $this->main_settings['city'],
+			'destination' => $package['destination']['destination_id'],
+			'weight'      => $weight,
+			'courier'     => $selected_couriers,
+			//  'originType' => 'city',
+			//  'destinationType' => 'subdistrict',
 
-    $args = array(
-      'origin' => $this->main_settings['city'],
-//      'originType' => 'city',
-      'destination' => $package['destination']['destination_id'],
-//      'destinationType' => 'subdistrict',
-      'weight' => $weight,
-      'courier' => $selected_couriers
-    );
+		);
 
-    // get the cost
-    $costs = $this->api->get_costs($args);
-    return $costs;
-  }
+		// get the cost
+		$costs = $this->api->get_costs( $args );
 
-  /*
-    Set the Rate based on Cost list from API
+		return $costs;
+	}
 
-    @param array $costs - Cost list from API
-  */
-  private function _set_rate($costs) {
-    // format the costs from API to WooCommerce
-    foreach($costs['results'] as $courier):
-      if(empty($courier) ) { break; }
+	/*
+	Set the Rate based on Cost list from API
 
-      // get full list of services
-      $code = $courier['code'];
-      $all_services = WCIS_Data::get_services($code);
+	@param array $costs - Cost list from API
+	*/
+	private function _set_rate( $costs ) {
+		// format the costs from API to WooCommerce
+		foreach ( $costs['results'] as $courier ):
+			if ( empty( $courier ) ) {
+				break;
+			}
 
-      // get allowed service from this courier
-      $setting_id = $code . '_services';
-      $allowed_services = isset($this->main_settings[$setting_id]) ? $this->main_settings[$setting_id] : array();
+			// get full list of services
+			$code         = $courier['code'];
+			$all_services = WCIS_Data::get_services( $code );
 
-      foreach($courier['costs'] as $service):
-        // check if this service is allowed
-        $is_allowed = false;
-        foreach($allowed_services as $as) {
-          // if has variation
-          if(isset($all_services[$as]['vars']) ) {
-            $is_allowed = in_array($service['service'], $all_services[$as]['vars'] );
-          }
-          else {
-            $is_allowed = $service['service'] === $as;
-          }
+			// get allowed service from this courier
+			$setting_id       = $code . '_services';
+			$allowed_services = isset( $this->main_settings[ $setting_id ] ) ? $this->main_settings[ $setting_id ] : array();
 
-          if($is_allowed) { break; }
-        }
+			foreach ( $courier['costs'] as $service ):
+				// check if this service is allowed
+				$is_allowed = false;
+				foreach ( $allowed_services as $as ) {
+					// if has variation
+					if ( isset( $all_services[ $as ]['vars'] ) ) {
+						$is_allowed = in_array( $service['service'], $all_services[ $as ]['vars'] );
+					} else {
+						$is_allowed = $service['service'] === $as;
+					}
 
-        if($is_allowed) {
-          $rate = array(
-            'id' => $code . '_' . strtolower($service['service']) . $this->instance_id,
-            'label' => strtoupper($code) . ' ' . $service['service'],
-            'cost' => $service['cost'][0]['value'],
-            'calc_tax' => 'per_order'
-          );
+					if ( $is_allowed ) {
+						break;
+					}
+				}
 
-          $this->add_rate($rate);
-        }
-      endforeach;
-    endforeach;
-  }
+				if ( $is_allowed ) {
+					$rate = array(
+						'id'       => $code . '_' . strtolower( $service['service'] ) . $this->instance_id,
+						'label'    => strtoupper( $code ) . ' ' . $service['service'],
+						'cost'     => $service['cost'][0]['value'],
+						'calc_tax' => 'per_order'
+					);
 
-  /*
-    Calculate the weight of items in cart. If all items has no weight specified, it will return 1.
+					$this->add_rate( $rate );
+				}
+			endforeach;
+		endforeach;
+	}
 
-    @param array - POST parameter
-    @return int - THe weight in the unit specified in admin.
-  */
-  private function _calculate_weight($package) {
-    global $woocommerce;
+	/*
+	Calculate the weight of items in cart. If all items has no weight specified, it will return 1.
 
-    $weight = wc_get_weight($woocommerce->cart->cart_contents_weight, 'g');
+	@param array - POST parameter
+	@return int - THe weight in the unit specified in admin.
+	*/
+	private function _calculate_weight( $package ) {
+		global $woocommerce;
 
-    if($weight > 0) {
-      return $weight;
-    }
-    // if no weight data, return default weight or 1kg
-    else {
-      $weight = (int) ceil(apply_filters('wcis_default_weight', $package) );
+		$weight = wc_get_weight( $woocommerce->cart->cart_contents_weight, 'g' );
 
-      return (is_int($weight) && $weight > 0 ) ? $weight : 1;
-    }
-  }
+		if ( $weight > 0 ) {
+			return $weight;
+		} // if no weight data, return default weight or 1kg
+		else {
+			$weight = (int) ceil( apply_filters( 'wcis_default_weight', $package ) );
 
-  /*
-    Get selected services from the courier
+			return ( is_int( $weight ) && $weight > 0 ) ? $weight : 1;
+		}
+	}
 
-    @return string - The courier format accepted by RajaOngkir, separated by semicolon (jne:tiki:pos)
-  */
-  private function _get_selected_couriers() {
-    $couriers = WCIS_Data::get_couriers();
+	/*
+	Get selected services from the courier
 
-    $selected_couriers = array();
-    foreach($couriers as $id => $name) {
-      if(!empty($this->main_settings[$id . '_services']) ) {
-        $selected_couriers[] = $id;
-      }
-    }
+	@return string - The courier format accepted by RajaOngkir, separated by semicolon (jne:tiki:pos)
+	*/
+	private function _get_selected_couriers() {
+		$couriers = WCIS_Data::get_couriers();
 
-    return join(':', $selected_couriers);
-  }
+		$selected_couriers = array();
+		foreach ( $couriers as $id => $name ) {
+			if ( ! empty( $this->main_settings[ $id . '_services' ] ) ) {
+				$selected_couriers[] = $id;
+			}
+		}
+
+		return join( ':', $selected_couriers );
+	}
 
 }
